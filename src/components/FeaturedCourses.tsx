6000ada33c581cc640fb7d1fd5968feb4a7e93a6
deepakguptabvp <deepakgupta.150743@gmail.com>
2024-11-09 14:23:13 +0530

    Working on Featured Courses

---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import courseData from "../data/music_courses.json"
+
+const FeaturedCourses = () => {
+  return (
+    <div className='py-12 bg-gray-900'>
+        <div>
+            <div className='text-center'>
+                <h2>Featured Courses</h2>
+                <p>Learn with the best</p>
+            </div>
+        </div>
+        <div>2</div>
+        <div>3</div>
+        
+    </div>
+  )
+}
+
+export default FeaturedCourses
