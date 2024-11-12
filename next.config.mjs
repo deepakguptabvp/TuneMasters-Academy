eb7943b7924a4fc9aaf4fdffa5f4c290ced81540
deepakguptabvp <deepakgupta.150743@gmail.com>
2024-11-12 13:46:09 +0530

    Vercel error changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -17,11 +18,6 @@ const nextConfig = {
         hostname: "img.wynk.in",
         pathname: "**",
       },
-      // {
-      //   protocol: "https",
-      //   hostname: "images.unsplash.com",
-      //   pathname: "**",
-      // },
     ],
   },
 };
