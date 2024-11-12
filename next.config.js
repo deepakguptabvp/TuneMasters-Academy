1a5627587905a167a7cb6101e9eeb1dabfc24888
deepakguptabvp <deepakgupta.150743@gmail.com>
2024-11-12 14:03:29 +0530

    Vercel error changes

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.js
-export const eslint = {
-    // This will allow production builds to complete even if
-    // there are ESLint errors in the project.
-    ignoreDuringBuilds: true,
-};
-  
